0f62d621b9107478649c2181a61c03fb1bd361d3
binuri <binurim95@gmail.com>
2022-06-17 15:51:58 +0530

    Add simple portfolio page created with react

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="title">Hello there! I'm Binuri</p>
+        <div className="row">
+          <a
+            className="icon"
+            href="https://www.linkedin.com/in/binuri-maleesha/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa fa-linkedin" aria-hidden="true"></i>
+          </a>
+          <a
+            className="icon"
+            href="https://github.com/Binurim"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa fa-github" aria-hidden="true"></i>
+          </a>
+          <a
+            className="icon"
+            href="https://www.instagram.com/binuri_/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+          </a>
+          <a
+            className="icon"
+            href="https://www.pinterest.com/binurim_/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa fa-pinterest" aria-hidden="true"></i>
+          </a>
+          <a
+            className="icon"
+            href="https://medium.com/@binurim"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa fa-medium" aria-hidden="true"></i>
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
