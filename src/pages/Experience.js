5ff880880b58a6421998efb0363ca4cbc86f2082
binuri <binurim95@gmail.com>
2025-11-08 13:56:15 +0530

    Update experience section

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,32 +1,46 @@
 const Experience = () => {
   const experienceData = [
+     {
+      id: 0,
+      time: '2025 Oct - Present',
+      position: 'Senior Software Engineer',
+      company: 'Qoria',
+      description: ''
+    },
+    // {
+    //   id: 1,
+    //   time: '2025 Jan - 2025 Aug',
+    //   position: 'Engineering Lead',
+    //   company: 'Persistent',
+    //   description: '',
+    // },
     {
-      id: 1,
-      time: '2024 - Present',
+      id: 2,
+      time: '2024 Jan - 2025 Aug',
       position: 'Lead Software Engineer',
       company: 'Persistent',
       description:
         'Develop enterprise-level UI platforms using Angular, RxJS, and SCSS. Engineered a major Angular version upgrade (v11 to v17), significantly enhancing performance and long-term maintainability. Worked with complex data flows by integrating RESTful APIs and managing backend-frontend interactions with Spring Boot. Mentored junior developers.',
     },
     {
-      id: 2,
-      time: '2023 - 2023 Dec',
+      id: 3,
+      time: '2023 Jan - 2023 Dec',
       position: 'Senior Software Engineer',
       company: 'Persistent',
       description:
         'Built and maintained shared Angular component libraries to drive modular and efficient development practices. Proficient in RxJS, Internationalization (i18n), and Progressive Web App (PWA) development. Maintained code quality by achieving unit test coverage and hands on with deployment processes by containerizing Angular applications with Docker and Nginx.',
     },
     {
-      id: 3,
-      time: '2021 - 2022 Dec',
+      id: 4,
+      time: '2021 May - 2022 Dec',
       position: 'Software Engineer',
       company: 'Persistent',
       description:
         'Developed responsive and user-centric front-end features using Angular and React/Redux. Expertly integrated RESTful APIs and managed complex form validations to ensure a seamless user experience. Committed to continuous learning by actively engaging in training on emerging technologies and UI/UX best practices.',
     },
     {
-      id: 4,
-      time: '2019 - 2020',
+      id: 5,
+      time: '2019 Aug - 2020 Feb',
       position: 'Software Engineer Intern',
       company: 'Pearson',
       description:
